d3181a779c5e76957e45664984057c79597b554e
kimura.m <k422756+git@gmail.com>
2020-09-10 00:44:57 +0900

    sqfliteの導入

--- lib/screens/top_page.dart
+++ lib/screens/top_page.dart
@@ -1,3 +1,4 @@
+import 'package:deadline_todo/utils/db_client.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatelessWidget {
@@ -5,6 +6,9 @@ class TopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var dbClient = DBClient();
+    dbClient.getDb();
+
     return Scaffold(
       body: Center(
         child: Text('hello world'),
---
+++ lib/utils/db_client.dart
@@ -0,0 +1,20 @@
+import 'package:sqflite/sqflite.dart';
+
+class DBClient {
+  Future<Database> getDb() async {
+    return await openDatabase('my_db.db', version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE todos (id INTEGER PRIMARY KEY AUTOINCREMENT, start TEXT, end TEXT, title TEXT);
+    ''');
+
+    await db.execute('''
+    INSERT INTO todos(start, end, title) VALUES (date('2020-12-15'), date('2020-12-30'), "test3");
+    ''');
+
+    var ret = await db.rawQuery('SELECT * FROM todos');
+    print(ret);
+  }
+}
