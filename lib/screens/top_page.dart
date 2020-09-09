fde273df77131551028db005a3a230d6dc2a826f
kimura.m <k422756+git@gmail.com>
2020-09-09 23:34:55 +0900

    トップページ雛形作成

---
+++ lib/screens/top_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatelessWidget {
+  static final route = "/";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello world'),
+      ),
+    );
+  }
+}
